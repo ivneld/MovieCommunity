a1df19d7432ed79f03810a1593d3a8536911b90c
youngseonseo <sue000731@naver.com>
2023-03-30 15:54:45 +0900

    rankingController, MovieNationType, RankingCode etc

--- src/main/java/Movie/MovieCommunity/domain/ranking/daily/DailyBoxOffice.java
+++ src/main/java/Movie/MovieCommunity/domain/ranking/daily/DailyBoxOffice.java
@@ -33,31 +33,31 @@ showCnt	문자열	해당일자에 상영된 횟수를 출력합니다.
  */
 @Data
 public class DailyBoxOffice {
-    private Long id;
-    private String boxofficeType;
-    private String showRange;
-    private String yearWeekTime;
+//    private Long id;
+//    private String boxofficeType;
+//    private String showRange;
+//    private String yearWeekTime;
     private String movieNm;	// 문자열	영화명(국문)을 출력합니다.
-    @NumberFormat
-    private Integer rnum;
+//    @NumberFormat
+//    private Integer rnum;
     @NumberFormat
     private Integer ranking;
     @NumberFormat
     private Integer rankInten;
-    private String rankOldAndNew;
-    private String movieCd;
+//    private String rankOldAndNew;
+//    private String movieCd;
     //movieNm	문자열	영화명(국문)을 출력합니다.
-    @DateTimeFormat(pattern = "yyyy-mm-dd")
-    private LocalDate openDt;
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Object openDt;
 
     //salesAmt	문자열	해당일의 매출액을 출력합니다.
     //salesShare	문자열	해당일자 상영작의 매출총액 대비 해당 영화의 매출비율을 출력합니다.
    // salesInten	문자열	전일 대비 매출액 증감분을 출력합니다.
    // salesChange	문자열	전일 대비 매출액 증감 비율을 출력합니다.
-    @NumberFormat(pattern = "###,###")
-    private Long salesAcc;
+//    @NumberFormat(pattern = "###,###")
+//    private Long salesAcc;
 
-    private MovieNationType movieNationType;
+//    private MovieNationType movieNationType;
    // private Long audiCnt;
   //  audiInten	문자열	전일 대비 관객수 증감분을 출력합니다.
   //  audiChange	문자열	전일 대비 관객수 증감 비율을 출력합니다.
@@ -70,11 +70,12 @@ public class DailyBoxOffice {
 
     }
 
-    public DailyBoxOffice(String movieNm, int ranking, int rankInten , int rnum ){
+    public DailyBoxOffice(String movieNm, int ranking, int rankInten, Object openDt, Long audiAcc ){
         this.movieNm=movieNm;
         this.ranking=ranking;
         this.rankInten=rankInten;
-        this.rnum=rnum;
+        this.openDt= openDt;
+        this.audiAcc=audiAcc;
     }
 
 
