57b10f06c5e53b28da97cbf3075fdeb015a85dbd
youngseonseo <sue000731@naver.com>
2023-06-15 22:21:09 +0900

    add function

--- src/main/java/Movie/MovieCommunity/advice/ApiControllerAdvice.java
+++ src/main/java/Movie/MovieCommunity/advice/ApiControllerAdvice.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiControllerAdvice {
 
