6b976e4a25cdbd0dbe74632648ebb0901999c42e
jeonghyun <invent819@naver.com>
2023-07-08 00:17:24 +0900

    DB 바꿈

--- src/main/java/Movie/MovieCommunity/dataCollection/MovieDataService.java
+++ src/main/java/Movie/MovieCommunity/dataCollection/MovieDataService.java
@@ -27,6 +27,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -80,13 +81,12 @@ public class MovieDataService {
     private HashMap<String, EtcData> etcData = new HashMap<>();
 
 
-//    @EventListener(ApplicationReadyEvent.class)
-    //@Transactional
-//    @PostConstruct
+    @Transactional
+    @PostConstruct
     public void Testing() throws Exception {
         System.out.println("key = " + key[0]);
         // 2018로 넘겨줄시 2018~2023 현재까지 조회(조회 순서는 최신 순)
-        movieDataCollection("2023");
+//        movieDataCollection("2023");
 
         // ! 주간 랭킹 데이터 있을경우만 startDate를 넣고 실행, 없으면 직접 넣기
 //        JpaWeeklyBoxOffice jpaWeeklyBoxOffice = weeklyBoxOfficeRepository.findLastByWeeklyId().orElseThrow(() -> new EntityNotFoundException("엔티티가 없습니다."));
@@ -95,7 +95,7 @@ public class MovieDataService {
 //        int month = Integer.parseInt(getDate.substring(4, 6));
 //        int day = Integer.parseInt(getDate.substring(6, 8));
 //        String startDate = LocalDate.of(year, month, day).plusWeeks(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-//        yearWeeklyBoxOfficeData(startDate);
+//        yearWeeklyBoxOfficeData("20230101");
         ///////////////////////////////////////////
 
 //        countEtc();         // 실행 전 메서드 주석 참고!
